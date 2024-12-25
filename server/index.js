49c910f2cc01344c3547a6ad2670caa1c373fef0
Ritika Mandal <s.ritika1705@gmail.com>
2024-12-25 12:37:32 +0530

    port changed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-const http = require('http')
-const express = require('express')
-const fs = require('fs/promises')
-const { Server: SocketServer } = require('socket.io')
-const path = require('path')
-const cors = require('cors')
+const http = require('http');
+const express = require('express');
+const fs = require('fs/promises');
+const { Server: SocketServer } = require('socket.io');
+const path = require('path');
+const cors = require('cors');
 const chokidar = require('chokidar');
-
-const pty = require('node-pty')
+const pty = require('node-pty');
 
 const ptyProcess = pty.spawn('bash', [], {
     name: 'xterm-color',
@@ -16,78 +15,80 @@ const ptyProcess = pty.spawn('bash', [], {
     env: process.env
 });
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const io = new SocketServer({
     cors: '*'
-})
+});
 
-app.use(cors())
+app.use(cors());
 
 io.attach(server);
 
 chokidar.watch('./user').on('all', (event, path) => {
-    io.emit('file:refresh', path)
+    io.emit('file:refresh', path);
 });
 
 ptyProcess.onData(data => {
-    io.emit('terminal:data', data)
-})
+    io.emit('terminal:data', data);
+});
 
 io.on('connection', (socket) => {
-    console.log(`Socket connected`, socket.id)
+    console.log(`Socket connected`, socket.id);
     console.log("INIT_CWD:", __dirname);
 
-
-    socket.emit('file:refresh')
+    socket.emit('file:refresh');
 
     socket.on('file:change', async ({ path, content }) => {
-        await fs.writeFile(`./user${path}`, content)
-    })
+        await fs.writeFile(`./user${path}`, content);
+    });
 
     socket.on('terminal:write', (data) => {
-        console.log('Term', data)
+        console.log('Term', data);
         ptyProcess.write(data);
-    })
-})
+    });
+});
 
 app.get('/files', async (req, res) => {
     const fileTree = await generateFileTree('./user');
-    return res.json({ tree: fileTree })
-})
+    return res.json({ tree: fileTree });
+});
 
 app.get('/files/content', async (req, res) => {
-    const path = req.query.path;
-    const content = await fs.readFile(`./user${path}`, 'utf-8')
-    return res.json({ content })
-})
+    const filePath = req.query.path;
+    const content = await fs.readFile(`./user${filePath}`, 'utf-8');
+    return res.json({ content });
+});
 
-if(process.env.NODE_ENV === 'production') {
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/dist')));
 }
 
-server.listen(9000, () => console.log(`🐳 Docker server running on port 9000`))
-
+// Dynamically bind to the port provided by Render
+server.listen(process.env.PORT || 9000, () => {
+    console.log(`🐳 Docker server running on port ${process.env.PORT || 9000}`);
+});
 
 async function generateFileTree(directory) {
-    const tree = {}
+    const tree = {};
 
     async function buildTree(currentDir, currentTree) {
-        const files = await fs.readdir(currentDir)
+        const files = await fs.readdir(currentDir);
 
         for (const file of files) {
-            const filePath = path.join(currentDir, file)
-            const stat = await fs.stat(filePath)
+            const filePath = path.join(currentDir, file);
+            const stat = await fs.stat(filePath);
 
             if (stat.isDirectory()) {
-                currentTree[file] = {}
-                await buildTree(filePath, currentTree[file])
+                currentTree[file] = {};
+                await buildTree(filePath, currentTree[file]);
             } else {
-                currentTree[file] = null
+                currentTree[file] = null;
             }
         }
     }
 
     await buildTree(directory, tree);
-    return tree
+    return tree;
 }
